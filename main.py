7566a62e5e3757bc46e11e159d6affbb71cf2d62
NUYHISNA <skt37525117@gmail.com>
2023-12-30 11:50:30 +0900

    todo리스트 입력 기능 개발

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import sqlite3
+from datetime import datetime
 
-def set_todo():
-    #cur.execute(f"INSERT INTO todo VALUES('{value[0]}','{value[1]}','{value[2]}','{value[3]}','{value[4]}')")
-    #con.commit()
-    pass
+def set_todo(con, cur):
+    today_date = datetime.today().strftime('%Y-%m-%d')
+    print("오늘의 할일을 입력하시오.")
+    todos = []
+    while(True):
+        todo_input = input()
+        if todo_input == 'exit':
+            print("프로그램 종료")
+            break
+        todos.append(todo_input)
+    print(todos)
+    # INSERT INTO todo('created_date', 'contents') VALUES ('2023-12-30', '베스킨라빈스먹기')
+    for todo in todos:
+        cur.execute(f"INSERT INTO todo('created_date', 'contents') VALUES('{today_date}','{todo}')")
+    con.commit()   
     
 def run():
     #프로그램 실행 시작점
@@ -14,6 +26,7 @@ def run():
         user_input = input("숫자를 입력해주세요")
         if user_input == '1':
             print("해야할일작성")
+            set_todo(con, cur)
         elif user_input == '2':
             print("완료표시")
         elif user_input == '3':
