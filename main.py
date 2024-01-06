e98500e776e5f543a27dbe4401a34dd8846b25fa
NUYHISNA <skt37525117@gmail.com>
2024-01-06 11:39:46 +0900

    todo 전체 조회 및 일자별 조회

--- main.py
+++ main.py
@@ -17,6 +17,26 @@ def set_todo(con, cur):
         cur.execute(f"INSERT INTO todo('created_date', 'contents') VALUES('{today_date}','{todo}')")
     con.commit()   
     
+def read_todo(cur):
+    #전체출력
+    result = cur.execute("SELECT seq, created_date, contents, finish_date, done FROM TODO")
+    todo = result.fetchall()
+    for data in todo:
+        print(f"번호: {data[0]}, 날짜: {data[1]}, 해야할 일: {data[2]}, 끝낸 날짜: {data[3]}, 실행한 횟수: {data[4]}")
+ 
+def date_todo(cur):
+    #특정날짜 투두리스트 출력
+    print("특정 날짜를 입력해주세요. ex)YYYY-mm-dd")
+    date_input = input()
+    if len(date_input) == 0:
+        date_input = datetime.today().strftime('%Y-%m-%d')
+    result = cur.execute(f"SELECT seq, created_date, contents, finish_date, done FROM TODO WHERE created_date='{date_input}'")
+    todo = result.fetchall()
+    if len(todo) == 0:
+        print("사용자의 값이 없습니다.") 
+    for data in todo:
+        print(f"번호: {data[0]}, 날짜: {data[1]}, 해야할 일: {data[2]}, 끝낸 날짜: {data[3]}, 실행한 횟수: {data[4]}")
+    
 def run():
     #프로그램 실행 시작점
     todo = True
@@ -28,9 +48,11 @@ def run():
             print("해야할일작성")
             set_todo(con, cur)
         elif user_input == '2':
-            print("완료표시")
+            print("전체출력")
+            read_todo(cur)
         elif user_input == '3':
             print("특정날짜 조회")
+            date_todo(cur)
         elif user_input == '4':
             print("해야할일수정")
         elif user_input == '5':
